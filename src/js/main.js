51c8c853c6cd7733548084efbcea7938168d2238
blackfxtalon <andyv@inbox.ru>
2021-05-24 15:00:07 +0500

    added js btn state

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
 /* main.js */
 import "./import/blocks";
 import "./import/components";
+
+// btn scripts
+const btns = document.querySelectorAll(".btn");
+
+const toggleBtnState = () => {
+    btns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            btn.classList.toggle("btn--pressed");
+            if (btn.matches(".btn--pressed")) {
+                btn.textContent = "Отменить запрос в сотрудники";
+            } else {
+                btn.textContent = "Работаете в этой компании?";
+            }
+        });
+    });
+};
+toggleBtnState();
